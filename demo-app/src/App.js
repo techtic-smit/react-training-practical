35996fe99e16e592d333f39c3fdb37ccfb8976f9
techtic-smit <smit@techtic.agency>
2023-03-31 18:42:43 +0530

    First Commit

---
+++ demo-app/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <label>Name:</label>
+        <br />
+        <input type="text" />
+        <br />
+
+        <label>Password:</label>
+        <br />
+        <input type="password" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default App;
