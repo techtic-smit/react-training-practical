767dadbc97da8ed72b47b384e98f6384d9113829
techtic-smit <108802629+techtic-smit@users.noreply.github.com>
2023-04-14 12:51:41 +0530

    Add files via upload

---
+++ react-training-practical-2/counter-app/src/App.js
@@ -0,0 +1,12 @@
+
+import Counter from "./Component/Counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter name="Techtic Solutions" />
+    </div>
+  );
+}
+
+export default App;
